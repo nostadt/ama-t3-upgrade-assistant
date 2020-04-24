6293ceff6041a7347f2d339c7d3b5142a2e53863
Alexander Nostadt <info.globetrotter.alex@gmail.com>
2020-04-24 21:25:25 +0200

    [TASK] Drop v7 and add v10 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
             'php' => '7.1.3-0.0.0'
         ],
     ],
