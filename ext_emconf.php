49de1f46079a28c5a67d56fcd69c01b4867667e2
Alexander Nostadt <alexander@nostadt.de>
2022-02-04 19:57:08 +0100

    TASK #45: Update typo3/cms-* to 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
             'php' => '7.4.0-8.1.99'
         ],
     ],
